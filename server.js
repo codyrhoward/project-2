dbc71c4a08699732356853bea806f3ec1ee2ccf0
codyrhoward <codyrhoward19@gmail.com>
2020-08-20 23:40:04 -0400

    finished file structure and wireframe

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(methodOverride('_method'));
-const mongoURI = process.env.MONGO_URI
+const mongoURI = process.env.mongo_URI
 // mongoose connection
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
