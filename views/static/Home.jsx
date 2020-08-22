bd92c5c35d47c695a11872bb68748dc7aefac4e8
codyrhoward <codyrhoward19@gmail.com>
2020-08-22 17:55:59 -0400

    added a static Home.jsx page and made it functional. Set Home page as default route.

---
+++ views/static/Home.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+
+class Home extends React.Component {
+
+  render(){
+    return(
+      <h1> This is the Home Page</h1>
+    )
+  }
+}
+
+module.exports = Home;
