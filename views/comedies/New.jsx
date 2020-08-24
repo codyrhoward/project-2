6dcf7030683c055d4bc9f103d76b9d67dda75fc2
codyrhoward <codyrhoward19@gmail.com>
2020-08-23 23:10:30 -0400

    New.jsx comedy  page styled

--- views/comedies/New.jsx
+++ views/comedies/New.jsx
@@ -5,19 +5,44 @@ class New extends React.Component {
     render() {
       return (
           <Default>
-          <div>
-              <h1>New Comedy</h1>
-              <form action="/comedies/" method="POST">
-                  Title: <input type="text" name="title" /><br/>                  
-                  Image: <input type="text" name="image" /><br/>                 
-                  Trailer: <input type="text" name="trailer"/><br/>  
-                  Synopsis: <br/> 
-                  <textarea type="text" name="synopsis" rows="10" cols="35"/><br/>
-                  Watched: <input type="checkbox" name="watched" /><br/>               
-                  <input type="submit" name="" value="Add Comedy"/><br/>
+              <br/>
+              <h1 className="new-h1 d-flex justify-content-center">New Comedy</h1>
+              <div className="new-container d-flex justify-content-center align-items-center">
+              
+           <div className="col-7">
+              <form className="whole-form" action="/comedies/" method="POST">
+              <div className="form-group"> 
+                <label for="title" >Title</label> 
+                  <input className="form-control" type="text" name="title"/>
+                  </div> 
+                  <div className="form-group">
+                    <label for="image">Image</label>          
+                    <input className="form-control"  type="text" name="image" />
+                  </div> 
+
+                  <div className="form-group"> 
+                  <label for="trailer">Trailer</label>              
+                  <input className="form-control"  type="text" name="trailer"/>
+                  </div>
+
+                  <div className="form-group"> 
+                  <label for="synopsis">Synopsis</label> 
+                  <textarea className="form-control"  type="text" name="synopsis" rows="10" cols="35"/>
+                  </div>
+
+                  <div classname="form-group">
+                  <input className="form-check-input" type="checkbox" name="watched"/>
+                  <label className="form-check-label" 
+                  for="watched">Watched</label>
+                  </div>
+                   
+                  <div className="form-group d-flex justify-content-end">       
+                  <input className="btn btn-primary" type="submit" name="" value="Add Comedy"/>
+                  </div>
                   
                </form>
-          </div>
+               </div>
+               </div>
           </Default>
           );
     }
